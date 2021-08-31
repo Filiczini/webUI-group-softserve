f07a48bbe5b9505a39b4b63cb6dc4a04e089fc1b
filiczini <filiczini@gmail.com>
2021-08-31 11:07:12 +0200

    update 1st task func

--- WEBUI Club/Sprint4/task1/index.js
+++ WEBUI Club/Sprint4/task1/index.js
@@ -7,11 +7,8 @@
 // ul.insertBefore(ul.children[4], ul.children[3]);
 
 // for (const i of ul.children) {
-//   i.textContent += ", ";
 //   string += i.textContent;
 // }
-
-// console.log(string.slice(0, string.length - 2));
 
 //2nd way
 const ul = document.getElementById("list");
@@ -22,9 +19,7 @@ ul.replaceChild(li[3], li[2]);
 ul.append(li[2]);
 
 ul.style.listStyle = "none";
-ul.style.display = "inline";
 
-for (const i of li) {
-  i.textContent += ", ";
-  i.style.display = "inline";
+for (const el of li) {
+  el.style.display = "inline";
 }
