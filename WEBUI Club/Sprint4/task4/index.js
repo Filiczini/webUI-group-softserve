391fbf5743089d5d4c2afc790af05f509a9f62bb
filiczini <filiczini@gmail.com>
2021-08-31 12:47:31 +0200

    rework functions,add html template to js,  add prevent default to a button

--- WEBUI Club/Sprint4/task4/index.js
+++ WEBUI Club/Sprint4/task4/index.js
@@ -3,36 +3,38 @@ const button = document.querySelector(".appeal-button");
 // define container with appeals
 const container = document.getElementsByClassName("appeals");
 
-// define function that
+// define function that create an Appeal structure, and add it to appeals container
 const addApeal = function (text) {
-  let article = document.createElement("article");
-  article.setAttribute("class", "fans-appeal");
-  let pText = document.createElement("p");
-  pText.textContent = text;
-  article.appendChild(pText);
-  const footer = document.createElement("footer");
-  footer.setAttribute("class", "flex-row");
-  article.appendChild(footer);
-  let pTime = document.createElement("p");
-  let pAuthor = document.createElement("p");
-  footer.appendChild(pTime);
-  footer.appendChild(pAuthor);
-  let timeText = document.createElement("time");
-  pTime.appendChild(timeText);
-  let authorName = document.createElement("a");
-  pAuthor.appendChild(authorName);
-  authorName.setAttribute("class", "appeal-author");
-  authorName.textContent = "FootballFan2000";
-  let time = new Date();
-  timeText.textContent = time.toLocaleString();
-  container[0].appendChild(article);
+  // create Date and format it
+  const date = new Date().toLocaleString();
+  //insert HTML template with data
+  container[0].insertAdjacentHTML(
+    "beforeend",
+    `<article class="fans-appeal">
+      <p>${text}</p>
+      <footer class="flex-row">
+          <p><time datetime="2018-10-10 20:33">${date}</time></p>
+          <p><a class="appeal-author"><b>FootballFan2000</b></a></p>
+      </footer>
+</article>`
+  );
 };
-
-function addAppealFunc() {
+// function to prevent the default behavior of form button
+function preventDef(event) {
+  event.preventDefault();
+  // call appealText function
+  appealText();
+}
+// function to read the textArea value and pass it to addAppeal function
+function appealText() {
+  // get textArea
   const textArea = document.getElementById("fan-appeal-text");
+  // read textArea text
   let textAreaAppeal = textArea.value;
+  // pass text to addAppeal function
   addApeal(textAreaAppeal);
+  // reload textArea text to empty string
   textArea.value = "";
 }
-
-button.addEventListener("click", addAppealFunc);
+// add eventListener to button
+button.addEventListener("click", preventDef);
