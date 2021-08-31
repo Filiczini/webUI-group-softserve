a264f5c08f52c653b2a41130fd20aa6342babf9e
filiczini <filiczini@gmail.com>
2021-08-31 17:49:51 +0200

    redefined p elements, remove display flex to li elements, wrap code to function

--- WEBUI Club/Sprint4/task2/index.js
+++ WEBUI Club/Sprint4/task2/index.js
@@ -1,21 +1,27 @@
-// define all variables. All html tags to work
-const h1 = document.querySelector("h1");
-const myDiv = document.querySelector("#myDiv");
-const myList = document.getElementById("myList");
-const span = document.getElementsByTagName("span");
+function addStyles() {
+  // define all variables. All html tags to work
+  // Change h1 backgound color
+  const h1 = document.querySelector("h1");
+  const myDiv = document.getElementById("myDiv");
+  const myList = document.getElementById("myList");
+  const span = document.getElementsByTagName("span");
+  const p = myDiv.querySelectorAll("p");
+  h1.style.backgroundColor = "#82ef7d";
 
-// Change h1 backgound color
-h1.style.backgroundColor = "#82ef7d";
+  // Change ul>li styles by using children collection
+  p[0].style.fontWeight = "bold";
+  p[1].style.color = "#ff0000";
+  p[2].style.textDecoration = "underline";
+  p[3].style.fontStyle = "italic";
 
-// Change ul>li styles by using children collection
-myDiv.children[0].style.fontWeight = "bold";
-myDiv.children[1].style.color = "#ff0000";
-myDiv.children[2].style.textDecoration = "underline";
-myDiv.children[3].style.fontStyle = "italic";
+  //add styles to every li element
+  for (const el of myList.children) {
+    el.style.cssText = "display: inline; float: left";
+  }
 
-//Define CSS styles by using multy-value method CSStext
-myList.style.cssText = "display: flex; list-style: none";
+  // Hide an element. Also we can use opacity 0.
+  // Display: none - is not a good practise, cause delete element from a flow
+  span[0].style.visibility = "hidden";
+}
 
-// Hide an element. Also we can use opacity 0.
-// Display: none - is not a good practise, cause delete element from a flow
-span[0].style.visibility = "hidden";
+addStyles();
