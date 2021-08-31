4e95b653f62ff11fa2a900af340aa156aef10a09
filiczini <filiczini@gmail.com>
2021-08-31 11:14:16 +0200

    add comments to task 1 function

--- WEBUI Club/Sprint4/task1/index.js
+++ WEBUI Club/Sprint4/task1/index.js
@@ -1,25 +1,35 @@
 // 1st way
 
+// // define unordered list and blank string
 // const ul = document.getElementById("list");
 // let string = "";
 
+// // changing order by swaping elements
 // ul.insertBefore(ul.children[4], ul.children[1]);
 // ul.insertBefore(ul.children[4], ul.children[3]);
 
+// // add every element to a string
 // for (const i of ul.children) {
 //   string += i.textContent;
 // }
 
-//2nd way
+// 2nd way
+// define unordered list and li elements
 const ul = document.getElementById("list");
 const li = document.querySelectorAll("li");
+
+// replace elements ta change the order
 ul.replaceChild(li[4], li[0]);
+// add elements instead of replaced
 ul.prepend(li[0]);
+// replace elements ta change the order
 ul.replaceChild(li[3], li[2]);
+// add elements instead of replaced
 ul.append(li[2]);
 
+// add styles to ul
 ul.style.listStyle = "none";
-
+// function to add display:inline to every li element
 for (const el of li) {
   el.style.display = "inline";
 }
