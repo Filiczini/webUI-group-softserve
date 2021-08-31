1d1afe64b0ac70bee6059ac0f72750387dc157c6
filiczini <filiczini@gmail.com>
2021-08-31 11:30:27 +0200

    rework task 3, add comments to code

--- WEBUI Club/Sprint4/task3/index.js
+++ WEBUI Club/Sprint4/task3/index.js
@@ -1,23 +1,33 @@
+// defining button and unordered list variables
 let button = document.getElementById("button");
 let ul = document.querySelector("ul");
 
-let tellMeWhy = function (string, e) {
-  let li = document.createElement("li");
-  li.textContent = string;
-
-  e.type == "mouseenter"
-    ? li.classList.add("green")
-    : e.type == "mouseleave"
-    ? li.classList.add("red")
-    : li.classList.add("blue");
-
-  ul.appendChild(li);
-
+// check the length of ul, if greater then 20, remove first child.
+let length = function () {
   if (ul.children.length >= 20) {
     ul.removeChild(ul.firstChild);
   }
 };
 
+// function accept string and event from eventListener
+let tellMeWhy = function (string, e) {
+  // creating li element
+  let li = document.createElement("li");
+  // add text from arguments to a li element
+  li.textContent = string;
+  // check event and add a class
+  e.type == "mouseenter"
+    ? li.classList.add("green")
+    : e.type == "mouseleave"
+    ? li.classList.add("red")
+    : li.classList.add("blue");
+  // adding li to the end of list
+  ul.appendChild(li);
+  // invoke length function
+  length();
+};
+
+// define button event listeners and invoke tellMeWhy func
 button.addEventListener("click", (e) => tellMeWhy("I was pressed", e));
 button.addEventListener("mouseenter", (e) => tellMeWhy("Mouse on me", e));
 button.addEventListener("mouseleave", (e) =>
