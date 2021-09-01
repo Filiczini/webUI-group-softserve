ae93d540b5d5a669a820a4a0e3b65d0f6efb8fc2
filiczini <filiczini@gmail.com>
2021-09-01 09:51:32 +0200

    rework sprint 4 task 4

--- WEBUI Club/Sprint4/task4/index.js
+++ WEBUI Club/Sprint4/task4/index.js
@@ -1,40 +1,43 @@
-// define button
-const button = document.querySelector(".appeal-button");
-// define container with appeals
-const container = document.getElementsByClassName("appeals");
+const addAppealToPage = function (button, container, textArea, template) {
+  function handleButtonClick(event) {
+    event.preventDefault();
 
-// define function that create an Appeal structure, and add it to appeals container
-const addApeal = function (text) {
-  // create Date and format it
-  const date = new Date().toLocaleString();
-  //insert HTML template with data
-  container[0].insertAdjacentHTML(
-    "beforeend",
-    `<article class="fans-appeal">
-      <p>${text}</p>
-      <footer class="flex-row">
-          <p><time datetime="2018-10-10 20:33">${date}</time></p>
-          <p><a class="appeal-author"><b>FootballFan2000</b></a></p>
-      </footer>
-</article>`
-  );
+    addValueToElement(textArea.value, container);
+    clearText(textArea);
+  }
+
+  function addValueToElement(value, element) {
+    if (value) {
+      // create Date and format it
+      const date = new Date().toLocaleString();
+      //insert HTML template with data
+      template = template.replace("%%%value%%%", value);
+      template = template.replaceAll("%%%date%%%", date);
+      element.insertAdjacentHTML("beforeend", template);
+    }
+  }
+  function clearText(el) {
+    el.value = "";
+  }
+  // add eventListener to button
+  button.addEventListener("click", handleButtonClick);
 };
-// function to prevent the default behavior of form button
-function preventDef(event) {
-  event.preventDefault();
-  // call appealText function
-  appealText();
-}
-// function to read the textArea value and pass it to addAppeal function
-function appealText() {
-  // get textArea
-  const textArea = document.getElementById("fan-appeal-text");
-  // read textArea text
-  let textAreaAppeal = textArea.value;
-  // pass text to addAppeal function
-  addApeal(textAreaAppeal);
-  // reload textArea text to empty string
-  textArea.value = "";
-}
-// add eventListener to button
-button.addEventListener("click", preventDef);
+
+const addCommentButton = document.getElementById("appeal-button");
+const comentsContainer = document.getElementById("appeals");
+const comentsTextArea = document.getElementById("fan-appeal-text");
+const commentsTemplate = `
+<article class="fans-appeal">
+  <p>%%%value%%%</p>
+  <footer class="flex-row">
+    <p><time datetime="%%%date%%%">%%%date%%%</time></p>
+    <p><a class="appeal-author"><b>FootballFan2000</b></a></p>
+  </footer>
+</article>`;
+
+addAppealToPage(
+  addCommentButton,
+  comentsContainer,
+  comentsTextArea,
+  commentsTemplate
+);
