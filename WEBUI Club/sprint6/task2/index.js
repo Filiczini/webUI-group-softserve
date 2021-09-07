53b4044d3364976d5ef513dbb20a3f62702e619e
filiczini <filiczini@gmail.com>
2021-09-07 13:29:48 +0200

    added 2nd task at sprint 6

---
+++ WEBUI Club/sprint6/task2/index.js
@@ -0,0 +1,33 @@
+class Person {
+  constructor(name, surname) {
+    (this.name = name), (this.surname = surname);
+  }
+  showFullName() {
+    return this.name + " " + this.surname;
+  }
+}
+
+class Student extends Person {
+  constructor(name, surname, year) {
+    super(name, surname), (this.year = year);
+  }
+  showFullName(midleName) {
+    return this.name + " " + this.surname + " " + midleName;
+  }
+  showCourse(year) {
+    let course = new Date().getFullYear() - this.year;
+
+    if (course >= 1 && course <= 6) {
+      return course;
+    } else if (course > 6) {
+      return "You are graduted.";
+    } else {
+      return "Year was not provided.";
+    }
+  }
+}
+
+const stud1 = new Student("Petro", "Petrenko", 2017);
+
+console.log(stud1.showFullName("Petrovych"));
+console.log("Current course: " + stud1.showCourse());
