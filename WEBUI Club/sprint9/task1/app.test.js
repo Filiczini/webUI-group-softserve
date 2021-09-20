82aae7a9113fc0b45868d780b461637cf2d62a69
filiczini <filiczini@gmail.com>
2021-09-21 01:31:53 +0200

    refactor

--- WEBUI Club/sprint9/task1/app.test.js
+++ WEBUI Club/sprint9/task1/app.test.js
@@ -1,9 +1,11 @@
 const sumFinder = require("./app");
 
-test("Takes 2 last nums from 233 and add them. Expect 3 + 3 = 6", () => {
-  expect(sumFinder(2, 233)).toBe(6);
-});
+describe("sumFinder", () => {
+  test("Takes 2 last nums from 233 and add them. Expect 3 + 3 = 6", () => {
+    expect(sumFinder(2, 233)).toBe(6);
+  });
 
-test("Takes 3 last nums of 2111. Expect 1 + 1 + 1 = 3", () => {
-  expect(sumFinder(3, 2111)).toBe(3);
+  test("Takes 3 last nums of 2111. Expect 1 + 1 + 1 = 3", () => {
+    expect(sumFinder(3, 2111)).toBe(3);
+  });
 });
