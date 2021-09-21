9a09367672e1dde8e76abe77530ad5074504cbdd
filiczini <filiczini@gmail.com>
2021-09-21 10:42:38 +0200

    refactor from 2 functions to 1

--- WEBUI Club/sprint9/task2/app.js
+++ WEBUI Club/sprint9/task2/app.js
@@ -1,27 +1,17 @@
-function numPower(n) {
-  let i = 0;
-  if (n > 0) {
-    while (2 ** i < n) {
-      i++;
-    }
-  } else {
-    return console.log("Enter positive Number");
-  }
-  return i - 1;
-}
-
 const findMersenne = function (n) {
   let resultArr = [];
-  for (let i = 1; i <= numPower(n); i++) {
-    let Mn = 2 ** i - 1;
+  i = 0;
+  let Mn = 2 ** i - 1;
+  do {
+    i++;
+    Mn = 2 ** i - 1;
     if (Mn <= n) {
       resultArr.push(Mn);
     }
-  }
+  } while (Mn < n);
   return resultArr;
 };
 
-module.exports = { numPower, findMersenne };
+module.exports = findMersenne;
 
-// findMersenne(200000);
-// numPower(-2);
+// findMersenne(2000000);
--- WEBUI Club/sprint9/task2/app.test.js
+++ WEBUI Club/sprint9/task2/app.test.js
@@ -1,14 +1,4 @@
-const { numPower, findMersenne } = require("./app");
-
-describe("numPower", () => {
-  test("Takes num and return max power of 2. 2000 is greater then 1024 but less then 2048. 1024 = 2 ** 10. Expect return 10", () => {
-    expect(numPower(2000)).toBe(10);
-  });
-
-  test("wer of 2. 1000 is greater then 512 but less then 1024. 512 = 2 ** 9. Expect return 9", () => {
-    expect(numPower(1000)).toBe(9);
-  });
-});
+const findMersenne = require("./app");
 
 describe("findMersenne", () => {
   test("Expect to return Mersenne numbers 1, 3, 7, 15, 31, 63, 127, 255, 511", () => {
